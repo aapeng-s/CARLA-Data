17dff0bb25b410f2b4630993560cf0af959d5e67
Z.R. ZHAO <garry@azurflow.com>
2024-10-21 21:35:52 +0800

    FEAT: add entrypoint argparse for semantic kitti

--- semantic_kitti.py
+++ semantic_kitti.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import argparse
 
 from packages.carla1s import CarlaContext, ManualExecutor, PassiveExecutor
 from packages.carla1s.actors import Vehicle, RgbCamera, DepthCamera, SemanticLidar
@@ -8,9 +9,16 @@ from packages.carla1s.tf import Transform
 from src.semantic_kitti import SemanticKittiDumper
 
 
-def main(*, fps: int = 20):
-    with CarlaContext(log_level=logging.DEBUG) as cc, ManualExecutor(cc, fixed_delta_seconds=1/fps) as exe:
-        cc.reload_world('Town01')
+def main(*, 
+         fps: int = 20, 
+         map: str = 'Town01', 
+         output: str = './temp/', 
+         host: str = 'localhost', 
+         port: int = 2000,
+         log_level: int = logging.DEBUG):
+
+    with CarlaContext(host=host, port=port, log_level=log_level) as cc, ManualExecutor(cc, fixed_delta_seconds=1/fps) as exe:
+        cc.reload_world(map_name=map)
         
         ego_vehicle: Vehicle = (cc.actor_factory
             .create(Vehicle, from_blueprint='vehicle.tesla.model3')
@@ -70,7 +78,7 @@ def main(*, fps: int = 20):
         exe.wait_sim_seconds(1)
         
         # SETUP DUMPER
-        dumper = SemanticKittiDumper('./temp/')
+        dumper = SemanticKittiDumper(output)
         dumper.bind_camera(cam_0_depth, data_folder="image_0")
         dumper.bind_camera(cam_1_depth, data_folder="image_1")
         dumper.bind_camera(cam_0_rgb, data_folder="image_2")
@@ -88,7 +96,18 @@ def main(*, fps: int = 20):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fps', type=int, default=20, help='Recommended FPS of the simulation')
+    parser.add_argument('--map', type=str, default='Town01', help='Name of the map to load')
+    parser.add_argument('--output', type=str, default='./temp/', help='Path to save the dataset')
+    parser.add_argument('--host', type=str, default='localhost', help='Host of the Carla server')
+    parser.add_argument('--port', type=int, default=2000, help='Port of the Carla server')
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode, setting log level to DEBUG')
+    args = parser.parse_args()
+    
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    
     try:
-        main()
+        main(fps=args.fps, map=args.map, output=args.output, host=args.host, port=args.port, log_level=log_level)
     except Exception:
         print(f'Exception occurred, check the log for more details.')
